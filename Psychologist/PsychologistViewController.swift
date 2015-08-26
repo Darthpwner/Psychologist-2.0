1d9dda522bd8c91f026627b8364066c0bcb52876
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-08-25 21:39:35 -0700

    My segues don't work, wtf

--- Psychologist/PsychologistViewController.swift
+++ Psychologist/PsychologistViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class PsychologistViewController: UIViewController {
+    
+    @IBAction func nothing(sender: UIButton) {
+        performSegueWithIdentifier("nothing", sender: nil)
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         var destination = segue.destinationViewController as? UIViewController
         if let navCon = destination as? UINavigationController {
@@ -19,7 +24,8 @@ class PsychologistViewController: UIViewController {
             if let identifier = segue.identifier {
                 switch identifier {
                     case "sad": hvc.happiness = 0
-                    case "sad": hvc.happiness = 100
+                    case "happy": hvc.happiness = 100
+                    case "nothing": hvc.happiness = 25
                     default: hvc.happiness = 50
                 }
             }
